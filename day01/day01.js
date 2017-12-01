015c7216048dd8949255e8942265aa9710396f9e
camiblanch <caminblanch@gmail.com>
2017-12-01 15:17:11 -0700

    Added test cases and completed test-case-0

--- day01/day01.js
+++ day01/day01.js
@@ -1,12 +1,19 @@
 var fs = require('fs');
 
-function getInput() {
-	fs.readFile('input.txt', 'utf8', function (err, data) {
-		if (err) {
-			return console.log(err);
-		}
-		return data;
-	});
-}
+fs.readFile('test-case-0.txt', 'utf8', function (err, input) {
+	if (err) {
+		return console.log(err);
+	}
+	var sum = 0;
 
-getInput();
+	for (var i = 0; i < input.length; i++) {
+		var currNumber = parseInt(input[i]);
+		var nextNumber = parseInt(input[i + 1]);
+
+		if (currNumber === nextNumber) {
+			sum += currNumber;
+		}
+	}
+
+	console.log("Sum is: " + sum);
+});
